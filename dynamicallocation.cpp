79ae893c758ba66cdd5f6fc736d7c17e1d72090c
swati singh 23 <singhswati8219860@gmail.com>
2024-09-15 22:08:31 +0530

    dynamic memory allocation

---
+++ dynamicallocation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int size ;
+    cout<<"enter number of elements";
+    cin>>size;
+
+    int *p=new int [size];
+
+    cout<<"Enter new size";
+    cin>>size;
+    p=new int[size];
+
+    return 0;
+
+}
+
